178aed7fdbb6ca9ba703a414fa9a4222911838c7
Ari Meyer <ari.f.meyer@gmail.com>
2023-10-17 14:21:48 -0500

    10th commit, finished Post endpoint in ReviewsController.

--- FullStackAuth_WebAPI/Controllers/ReviewsController.cs
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -1,5 +1,9 @@
 ﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.DataTransferObjects;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,24 +26,69 @@ namespace FullStackAuth_WebAPI.Controllers
         {
             try
             {
-                var reviews = _context.Reviews.Select(r => new ReviewWit)
+                var reviews = _context.Reviews.Select(r => new ReviewWithUserDto
+                {
+                    Id = r.Id,
+                    BookId = r.BookId,
+                    Text = r.Text,
+                    Rating = r.Rating,
+                    User = new UserForDisplayDto
+                    {
+                        Id = r.User.Id,
+                        FirstName = r.User.FirstName,
+                        LastName = r.User.LastName,
+                        UserName = r.User.UserName,
+                    }
+                }).ToList();
+
+                return StatusCode(200, reviews);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
             }
         }
 
         // GET api/<ReviewsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ReviewsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
         
+
+        // POST api/reviews
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Review data)
+        {
+            try
+            {
+                string userId = User.FindFirstValue("id");
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized();
+                }
+
+                data.UserId = userId;
+
+                _context.Reviews.Add(data);
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _context.SaveChanges();
+
+                return StatusCode(201, data);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
+
+        // PUT api/<ReviewsController>/5
+        
+
+        // DELETE api/<ReviewsController>/5
+       
+        
     }
 }
