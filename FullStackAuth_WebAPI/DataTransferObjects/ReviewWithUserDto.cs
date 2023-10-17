e4778e50d2975f44254b8f3d576e4be37d8bba22
Ari Meyer <ari.f.meyer@gmail.com>
2023-10-17 13:40:20 -0500

    7th commit, added ReviewsController.

--- FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDto.cs
@@ -1,13 +1,17 @@
-﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+﻿using FullStackAuth_WebAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.DataTransferObjects
 {
     public class ReviewWithUserDto
     {
         public int Id { get; set; }
-
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
         public string BookId { get; set; }
-
         public string Text {  get; set; }
-
         public double Rating { get; set; }
+        public UserForDisplayDto User {  get; set; }
     }
 }
